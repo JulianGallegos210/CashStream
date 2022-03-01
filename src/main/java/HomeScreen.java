cfd37269f0b8d6d00cbb013850b117fe3f2d97b8
Gallegos <Gallegos@DESKTOP-P1UHLT3.lan>
2022-03-01 00:14:57 -0600

    for a future reference

--- src/main/java/HomeScreen.java
+++ src/main/java/HomeScreen.java
@@ -1,17 +1,23 @@
+
+import java.util.Scanner;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
 /**
- *
+ *Just a start
  * @author Gallegos
  */
 public class HomeScreen {
     public static void main(String[] args) {
-        // Idea for a start
+        Scanner scn = new Scanner(System.in);
+        String user, password;
         System.out.println("Cash$tream");
-        System.out.println("Username");
-        System.out.println("Password");
+        System.out.println("Enter Username");
+        user = scn.nextLine();
+        System.out.println("Enter Password");
+        password = scn.nextLine();
     }
 }
