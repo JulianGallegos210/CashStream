361abe1fcef2790d4d45ed87445fb69f84df14ef
Gallegos <Gallegos@DESKTOP-P1UHLT3.lan>
2022-03-31 17:40:31 -0500

    for a future reference

--- src/main/java/HomeScreen.java
+++ src/main/java/HomeScreen.java
@@ -9,12 +9,6 @@ import java.util.Scanner;
  */
 public class HomeScreen {
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        String user, password;
-        System.out.println("Cash$tream");
-        System.out.println("Enter Username");
-        user = scn.nextLine();
-        System.out.println("Enter Password");
-        password = scn.nextLine();
+        new HomeScreenInterface().setVisible(true);
     }
 }
