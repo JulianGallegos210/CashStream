5270bb78d1286435bac177ff945abcdad223ffe5
Gallegos <Gallegos@DESKTOP-P1UHLT3.lan>
2022-03-31 17:19:22 -0500

    for a future reference

--- src/main/java/AccountClass.java
+++ src/main/java/AccountClass.java
@@ -23,10 +23,51 @@ public class AccountClass {
     double withdraw(double ammount){
         return checkingBalance-ammount;
     }
-    double getSavingBalance(){
-        return savingBalance;
+    public String getFirstName() {
+        return firstName;
     }
-    double getCheckingBalance(){
-        return checkingBalance;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getActNumber() {
+        return actNumber;
+    }
+
+    public void setActNumber(int actNumber) {
+        this.actNumber = actNumber;
+    }
+
+    public int getRouteNumber() {
+        return routeNumber;
+    }
+
+    public void setRouteNumber(int routeNumber) {
+        this.routeNumber = routeNumber;
     }
 }
