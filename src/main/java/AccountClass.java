c4cd2b6aa7cb07f1fe443aa0158713541dc6e915
Gallegos <Gallegos@DESKTOP-P1UHLT3.lan>
2022-03-31 15:05:05 -0500

    for a future reference

---
+++ src/main/java/AccountClass.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Gallegos
+ */
+public class AccountClass {
+    String firstName;
+    String lastName;
+    String username;
+    String password;
+    int actNumber;
+    int routeNumber;
+    double savingBalance;
+    double checkingBalance;
+}
