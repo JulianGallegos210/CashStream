19d249016e8baa15746d863376694e2284254eb6
Gallegos <Gallegos@DESKTOP-P1UHLT3.lan>
2022-04-07 15:59:31 -0500

    Added Withdraw and Deposit methods

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -16,12 +19,45 @@ public class Account {
     private int routeNumber;
     private double savingBalance;
     private double checkingBalance;
-    
-    double deposit(double ammount){
-        return ammount+checkingBalance;
+    // Account constructor class class will be given account number and routing number
+    Account(String firstName, String lastName, String username, String password){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
     }
-    double withdraw(double ammount){
-        return checkingBalance-ammount;
+    public void deposit(double ammount){
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Type 1 to deposit into your checking,2 to deposit into your savings, and 3 if you would like to cancel: ");
+        int x= scn.nextInt();
+        if(x==1){
+            checkingBalance += ammount;
+        }else if(x==2){
+            savingBalance += ammount;
+        }else{
+            System.out.println("No money was added to your account.");
+        }
+        
+    }
+    public void withdraw(double ammount){
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Type 1 to withdraw from your checking,2 to withdraw your savings, and 3 if you would like to cancel: ");
+        int x= scn.nextInt();
+        if(x==1){
+            checkingBalance -= ammount;
+        }else if(x==2){
+            savingBalance -= ammount;
+        }else{
+            System.out.println("No money was taken from your account.");
+        }
+        
+    }
+
+    public double getSavingBalance() {
+        return savingBalance;
+    }
+    public double getCheckingBalance() {
+        return checkingBalance;
     }
     public String getFirstName() {
         return firstName;
