5dfe09c8c490f91817602d65b44fa080744c4372
Gallegos <Gallegos@DESKTOP-P1UHLT3.lan>
2022-04-07 20:52:08 -0500

    Added a sendMoney method

---
+++ src/main/java/DBOperations.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Gallegos
+ */
+public class DBOperations {
+    public void create(){
+        
+    }
+}
